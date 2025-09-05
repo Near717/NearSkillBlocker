ad322f0754c9a7840824fbc17e59ec0bf435162b
Void <66706132+Near717@users.noreply.github.com>
2025-09-05 00:19:41 -0300

    Updated versions

--- NearSkillBlocker.lua
+++ NearSkillBlocker.lua
@@ -2,7 +2,7 @@ NEAR_SB = {
 	name = "NearSkillBlocker",
 	title = "Near's Skill Blocker",
 	shortTitle = "Skill Blocker",
-	version = "3.9.1",
+	version = "3.10.0",
 	author = "|cCC99FFnotnear|r",
 }
 
